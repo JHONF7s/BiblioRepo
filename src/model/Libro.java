54f0f4eb307e8d861390e6f89de39a2edb28a839
Victor-Sheen-U <arias.victor.9253@eam.edu.co>
2026-03-02 16:46:36 -0500

    Añadiendo atributos basicos en libro

--- src/model/Libro.java
+++ src/model/Libro.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Libro {
+
+    private int id;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private int anoPublicacion;
+    private String categoria;
+    private int estado; // 0 = disponible, 1 = reservador, 2 = retirado
+    
 }
