7fd6a802ac35b88d280bd94341912a0686eb7761
JHONF7s <jhonf5202@gmail.com>
2026-03-02 23:00:31 +0000

    Controladores y clases

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -14,4 +14,44 @@ public class Reserva {
         this.libro = libro;
         date = LocalDateTime.now();
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void cancelar(){
+        cliente.setEstado(0);
+        libro.setEstado(0);
+        cliente = null;
+        libro = null;
+        date = null;
+    }
 }
