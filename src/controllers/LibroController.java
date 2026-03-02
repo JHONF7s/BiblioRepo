7eda01e509945db55eb833e0ea6f1256bc258cc4
Victor-Sheen-U <arias.victor.9253@eam.edu.co>
2026-03-02 17:52:29 -0500

    Añadiendo metodo de agregar libros en el controlador

--- src/controllers/LibroController.java
+++ src/controllers/LibroController.java
@@ -5,14 +5,20 @@ import model.Libro;
 import java.util.ArrayList;
 
 public class LibroController {
-    private ArrayList<Libro> libros;
+    private ArrayList<Libro> listaLibros;
 
     public LibroController() {
-        this.libros = new ArrayList<>();
+        this.listaLibros = new ArrayList<>();
     }
 
-    public boolean agregarLibro(){
-        return false;
+    public boolean agregarLibro(Libro libro){
+        for (Libro libros : this.listaLibros){
+            if (libros.getId() == libro.getId()){
+                return false;
+            }
+        }
+        this.listaLibros.add(libro);
+        return true;
     }
 
     public boolean eliminarLibro(){
