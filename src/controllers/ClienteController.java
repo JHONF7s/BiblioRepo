f5254e291db7a9aef85584fecd434587159e8fda
JHONF7s <jhonf5202@gmail.com>
2026-03-02 22:32:32 +0000

    nuevos controladores

--- src/controllers/ClienteController.java
+++ src/controllers/ClienteController.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import model.Cliente;
+
+import java.util.ArrayList;
+
 public class ClienteController {
+    private ArrayList<Cliente> clientes;
+
+    public ClienteController() {
+        this.clientes = new ArrayList<>();
+    }
+
+    public boolean agregarCliente(){
+        return false;
+    }
+
+    public boolean eliminarCliente(){
+
+        return false;
+    }
+
+    public boolean modificarCliente(){
+
+        return false;
+    }
+
+    public Cliente buscarCliente(){
+        return null;
+    }
 }
+
--- src/controllers/LibroController.java
+++ src/controllers/LibroController.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import model.Libro;
+
+import java.util.ArrayList;
+
 public class LibroController {
+    private ArrayList<Libro> libros;
+
+    public LibroController() {
+        this.libros = new ArrayList<>();
+    }
+
+    public boolean agregarLibro(){
+        return false;
+    }
+
+    public boolean eliminarLibro(){
+
+        return false;
+    }
+
+    public boolean modificarLibro(){
+
+        return false;
+    }
+
+    public Libro buscarLibro(){
+        return null;
+    }
 }
+
